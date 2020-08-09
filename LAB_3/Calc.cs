a350ab176e3c760fa28d9a028ebb042f0459e7cb
shubham7226 <60355904+shubham7226@users.noreply.github.com>
2020-08-09 19:28:51 +0530

    Add files via upload

---
+++ LAB_3/Calc.cs
@@ -0,0 +1,16 @@
+using System;
+namespace prog1
+{
+	public class prog1_class
+	{
+		public int multiplication(int x,int y)
+		{
+			return x*y;
+		}
+
+		public int addition(int x,int y)
+		{
+			return x+y;
+		}
+	}
+}
---
+++ LAB_3/NewCalc.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Reflection;
+[assembly: AssemblyVersion("1.0.0.0")]
+namespace prog1
+{
+	public class prog1_class
+	{
+		public int multiplication(int x,int y)
+		{
+			Console.WriteLine("This is a new Version");
+			return x*y;
+		}
+
+		public int addition(int x,int y)
+		{
+			Console.WriteLine("This is anew Version");
+			return x+y;
+		}
+	}
+}
